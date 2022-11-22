0b58f8cb0d7d30fd3c7c96cbdd3bc51e81a8e3be
Cassandra Rousseau <cass201001@gmail.com>
2022-11-21 21:43:57 -0500

    Fixed UI display, working on UI interaction

--- Assets/Codes/DialogueOption.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DialogueOption 
-{
-    public string buttonText;
-    public UnityEvent actionToTrigger;
-}
